74df794f0d0752cafb61d56a4a2aad2dc4b36068
Mirko <mirkosbaglia2001@gmail.com>
2021-03-18 15:35:29 +0100

    Esercizio Terminato

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// GOAL: Creare un hamburger menu con l'utilizzo di Html, Css e JavaScript. Scrivere solo la parte con jQuery senza modiﬁcare HTML e CSS.
+// N.B.: nessuna modifica consentita a HTML o CSS
+
+
+
+function mostraHamb(){
+ $('.hamburger-menu').show();
+}
+
+function togliHamb(){
+ $('.hamburger-menu').hide();
+}
+
+
+//Metodo 1
+// function init() {
+//   $('.fas.fa-bars').click(function() {
+//     mostraHamb();
+//   });
+//
+//   $('.fas.fa-times').click(function() {
+//     togliHamb();
+//   });
+// }
+//
+// init();
+//
+
+
+//Metodo 2
+
+function init() {
+  $('.fas.fas-bars').click(mostraHamb);
+  $('.fas.fa-times').click(togliHamb);
+
+
+}
+
+init();
